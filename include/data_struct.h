2a74e0740759c987b351ee7dced67ccd973cb435
naturinari <gael.maestre@gmail.com>
2016-05-28 18:05:25 +0200

    Planet structure
    First implementation of the structure used for the planetes.

---
+++ include/data_struct.h
@@ -0,0 +1,8 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+typedef struct planete{
+	//long poids;//il faut trouver une representation utilisable du poids (du 10^24 ca rentre pas dans les variables)
+	int vitesse_x, vitesse_y;
+	long position_x, position_y;
+}
